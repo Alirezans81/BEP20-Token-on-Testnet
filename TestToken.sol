b8b1cf8feaf0accfccd36f8fc9e3054a88f390d4
Alirezans81 <99117010+Alirezans81@users.noreply.github.com>
2022-04-19 21:41:52 +0430

    Add files via upload

---
+++ TestToken.sol
@@ -0,0 +1,11 @@
+// contracts/BEP20.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TestToken", "TET") {
+        _mint(msg.sender, initialSupply);
+    }
+}
